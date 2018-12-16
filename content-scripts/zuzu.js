5b28f83ac06b571e1e8116ebf3b660b12e2fc04a
Aung Myo Kyaw <git@aungmyokyaw.com>
2018-12-16 17:48:48 +0630

    chore: prettier

--- content-scripts/zuzu.js
+++ content-scripts/zuzu.js
@@ -1,17 +1,17 @@
 //creating textarea for storing clipboard data;
 var inputText = document.createElement('TEXTAREA');
 document.body.appendChild(inputText);
-inputText.style.display= "block";
-inputText.style.position= "fixed";
-inputText.style.left= "0";
-inputText.style.top= "0";
-inputText.style.width= "0";
-inputText.style.height= "0";
-inputText.style.border= "none";
-inputText.style.overflow= "auto";
-inputText.style.outline= "none";
-inputText.style.resize= "none";
-inputText.style.opacity= "0";
+inputText.style.display = 'block';
+inputText.style.position = 'fixed';
+inputText.style.left = '0';
+inputText.style.top = '0';
+inputText.style.width = '0';
+inputText.style.height = '0';
+inputText.style.border = 'none';
+inputText.style.overflow = 'auto';
+inputText.style.outline = 'none';
+inputText.style.resize = 'none';
+inputText.style.opacity = '0';
 
 //iszg ismm
 var isZg = false;
@@ -24,110 +24,122 @@ var uniHeader = 'Unicode';
 var zgHeader = 'Zawgyi';
 var disabledHeader = false;
 
-function getSettingData(){
-	//magic on/off
-	chrome.storage.local.get('magic',(result)=>{
-		magic = result.magic!==undefined ? result.magic : magic;
-	});
+function getSettingData() {
+  //magic on/off
+  chrome.storage.local.get('magic', result => {
+    magic = result.magic !== undefined ? result.magic : magic;
+  });
 
-	//zgFirst on/off
-	chrome.storage.local.get('zgFirst',(result)=>{
-		zgFirst = result.zgFirst!==undefined ? result.zgFirst : zgFirst;
-	});
+  //zgFirst on/off
+  chrome.storage.local.get('zgFirst', result => {
+    zgFirst = result.zgFirst !== undefined ? result.zgFirst : zgFirst;
+  });
 
-	//header on/off
-	chrome.storage.local.get('disabledHeader',(result)=>{
-		disabledHeader = result.disabledHeader!==undefined ? result.disabledHeader : disabledHeader;
-	});
+  //header on/off
+  chrome.storage.local.get('disabledHeader', result => {
+    disabledHeader =
+      result.disabledHeader !== undefined
+        ? result.disabledHeader
+        : disabledHeader;
+  });
 
-	//set Uni Header
-	chrome.storage.local.get('uniHeader',(result)=>{
-		uniHeader = result.uniHeader!==undefined ? result.uniHeader : uniHeader;
-	});
+  //set Uni Header
+  chrome.storage.local.get('uniHeader', result => {
+    uniHeader = result.uniHeader !== undefined ? result.uniHeader : uniHeader;
+  });
 
-	//set Zawgyi Header
-	chrome.storage.local.get('zgHeader',(result)=>{
-		zgHeader = result.zgHeader!==undefined ? result.zgHeader : zgHeader;
-	});
+  //set Zawgyi Header
+  chrome.storage.local.get('zgHeader', result => {
+    zgHeader = result.zgHeader !== undefined ? result.zgHeader : zgHeader;
+  });
 
-	chrome.storage.local.set({
-		change:false
-	});
+  chrome.storage.local.set({
+    change: false
+  });
 }
 
 getSettingData();
 
 //convertor
-function convert(text){
-	if(magic){
-		if(isZg && isMM){//the text is zawgyi
-			if(disabledHeader){
-				inputText.value =
-				`${inputText.value}\n\n${Rabbit.zg2uni(inputText.value)}`
-			} else {
-				inputText.value =
-				`[${zgHeader}]\n${inputText.value}\n\n[${uniHeader}]\n${Rabbit.zg2uni(inputText.value)}`
-			}
-		} else if(isMM && !zgFirst){//zg first is off
-				if(disabledHeader){
-					inputText.value =
-					`${inputText.value}\n\n${Rabbit.uni2zg(inputText.value)}`
-				} else {
-					inputText.value =
-					`[${uniHeader}]\n${inputText.value}\n\n[${zgHeader}]\n${Rabbit.uni2zg(inputText.value)}`
-				}
-		} else {//zg first is on
-			if(disabledHeader){
-				inputText.value =
-				`${Rabbit.uni2zg(inputText.value)}\n\n${inputText.value}`
-			} else {
-				inputText.value =
-				`[${zgHeader}]\n${Rabbit.uni2zg(inputText.value)}\n\n[${uniHeader}]\n${inputText.value}`
-			}
-		}
-	} else {
-		if(isZg && isMM){
-			inputText.value = `${Rabbit.zg2uni(inputText.value)}`;
-		} else if(isMM){
-			inputText.value = `${Rabbit.uni2zg(inputText.value)}`;
-		}
-	}
+function convert(text) {
+  if (magic) {
+    if (isZg && isMM) {
+      //the text is zawgyi
+      if (disabledHeader) {
+        inputText.value = `${inputText.value}\n\n${Rabbit.zg2uni(
+          inputText.value
+        )}`;
+      } else {
+        inputText.value = `[${zgHeader}]\n${
+          inputText.value
+        }\n\n[${uniHeader}]\n${Rabbit.zg2uni(inputText.value)}`;
+      }
+    } else if (isMM && !zgFirst) {
+      //zg first is off
+      if (disabledHeader) {
+        inputText.value = `${inputText.value}\n\n${Rabbit.uni2zg(
+          inputText.value
+        )}`;
+      } else {
+        inputText.value = `[${uniHeader}]\n${
+          inputText.value
+        }\n\n[${zgHeader}]\n${Rabbit.uni2zg(inputText.value)}`;
+      }
+    } else {
+      //zg first is on
+      if (disabledHeader) {
+        inputText.value = `${Rabbit.uni2zg(inputText.value)}\n\n${
+          inputText.value
+        }`;
+      } else {
+        inputText.value = `[${zgHeader}]\n${Rabbit.uni2zg(
+          inputText.value
+        )}\n\n[${uniHeader}]\n${inputText.value}`;
+      }
+    }
+  } else {
+    if (isZg && isMM) {
+      inputText.value = `${Rabbit.zg2uni(inputText.value)}`;
+    } else if (isMM) {
+      inputText.value = `${Rabbit.uni2zg(inputText.value)}`;
+    }
+  }
 }
 
 //start when user select all text
-document.addEventListener('keydown',(e)=>{
-	var isCtrlPressed = e.ctrlKey || e.metaKey;
-	var isSelectAll = isCtrlPressed && e.keyCode===65;
-	if(isSelectAll){
-		// get active element
-		var active = document.activeElement;
+document.addEventListener('keydown', e => {
+  var isCtrlPressed = e.ctrlKey || e.metaKey;
+  var isSelectAll = isCtrlPressed && e.keyCode === 65;
+  if (isSelectAll) {
+    // get active element
+    var active = document.activeElement;
 
-		//cut and paste to textarea clipboard
-		inputText.value = '';
-		// document.execCommand('selectAll');
-		document.execCommand('cut');
-		inputText.focus();
-		document.execCommand('paste');
+    //cut and paste to textarea clipboard
+    inputText.value = '';
+    // document.execCommand('selectAll');
+    document.execCommand('cut');
+    inputText.focus();
+    document.execCommand('paste');
 
-		//change textarea clipboard data and cut to clipboard
-		isZg = isZawgyi(inputText.value);
-		isMM = isMyanmar(inputText.value);
+    //change textarea clipboard data and cut to clipboard
+    isZg = isZawgyi(inputText.value);
+    isMM = isMyanmar(inputText.value);
 
-		//checking change
-		chrome.storage.local.get('change',(result)=>{
-			var isChanged = result.change!==undefined ? result.change : false;
-			if(isChanged){
-				getSettingData();
-			}
-		});
-		//convert
-		convert();
+    //checking change
+    chrome.storage.local.get('change', result => {
+      var isChanged = result.change !== undefined ? result.change : false;
+      if (isChanged) {
+        getSettingData();
+      }
+    });
+    //convert
+    convert();
 
-		inputText.select();
-		document.execCommand('cut');
+    inputText.select();
+    document.execCommand('cut');
 
-		//paste to active element
-		active.focus();
-		// document.execCommand('paste');
-	}
+    //paste to active element
+    active.focus();
+    // document.execCommand('paste');
+  }
 });
--- options/options.js
+++ options/options.js
@@ -1,9 +1,9 @@
-var magicSwitch = document.getElementsByClassName('magic')
+var magicSwitch = document.getElementsByClassName('magic');
 var magicOn = document.getElementById('magicon');
 var magicOff = document.getElementById('magicoff');
 var MagicOption = document.getElementById('MagicOption');
 var zgFirstElement = document.getElementById('zgFirst');
-var uniHeaderElement =	document.getElementById('uniHeader');
+var uniHeaderElement = document.getElementById('uniHeader');
 var zgHeaderElement = document.getElementById('zgHeader');
 var disabledHeaderElement = document.getElementById('disabledHeader');
 var header = document.getElementById('header');
@@ -15,117 +15,121 @@ var zgHeader = 'Zawgyi';
 var disabledHeader = false;
 
 //change true
-function changeTrue(){
-	chrome.storage.local.set({
-		change:true
-	});
+function changeTrue() {
+  chrome.storage.local.set({
+    change: true
+  });
 }
 
 //setting data
-chrome.storage.local.get('magic',(result)=>{
-	//get magic on off data
-	magic = result.magic!==undefined ? result.magic : magic;
-	//set checked
-	if(magic){//magic on
-		magicOn.setAttribute('checked',true);
-		MagicOption.style.display = 'block';
-	} else {
-		magicOff.setAttribute('checked',true);
-		MagicOption.style.display = 'none';
-	}
+chrome.storage.local.get('magic', result => {
+  //get magic on off data
+  magic = result.magic !== undefined ? result.magic : magic;
+  //set checked
+  if (magic) {
+    //magic on
+    magicOn.setAttribute('checked', true);
+    MagicOption.style.display = 'block';
+  } else {
+    magicOff.setAttribute('checked', true);
+    MagicOption.style.display = 'none';
+  }
 });
 
-chrome.storage.local.get('zgFirst',(result)=>{
-	zgFirst = result.zgFirst!==undefined ? result.zgFirst : zgFirst;
-	if(zgFirst){
-		zgFirstElement.setAttribute('checked',true);
-	} else {
-		zgFirstElement.removeAttribute('checked');
-	}
+chrome.storage.local.get('zgFirst', result => {
+  zgFirst = result.zgFirst !== undefined ? result.zgFirst : zgFirst;
+  if (zgFirst) {
+    zgFirstElement.setAttribute('checked', true);
+  } else {
+    zgFirstElement.removeAttribute('checked');
+  }
 });
 
-chrome.storage.local.get('uniHeader',(result)=>{
-	uniHeader = result.uniHeader!==undefined ? result.uniHeader : uniHeader;
-	uniHeaderElement.setAttribute('placeholder',uniHeader);
+chrome.storage.local.get('uniHeader', result => {
+  uniHeader = result.uniHeader !== undefined ? result.uniHeader : uniHeader;
+  uniHeaderElement.setAttribute('placeholder', uniHeader);
 });
 
-chrome.storage.local.get('zgHeader',(result)=>{
-	zgHeader = result.zgHeader!==undefined ? result.zgHeader : zgHeader;
-	zgHeaderElement.setAttribute('placeholder',zgHeader);
+chrome.storage.local.get('zgHeader', result => {
+  zgHeader = result.zgHeader !== undefined ? result.zgHeader : zgHeader;
+  zgHeaderElement.setAttribute('placeholder', zgHeader);
 });
 
 //magic switch on/off
-magicOn.addEventListener('click',(e)=>{
-	chrome.storage.local.set({
-		magic:true
-	});
-	changeTrue();
-	MagicOption.style.display = 'block';
+magicOn.addEventListener('click', e => {
+  chrome.storage.local.set({
+    magic: true
+  });
+  changeTrue();
+  MagicOption.style.display = 'block';
 });
-magicOff.addEventListener('click',(e)=>{
-	chrome.storage.local.set({
-		magic:false
-	});
-	changeTrue();
-	MagicOption.style.display = 'none';
+magicOff.addEventListener('click', e => {
+  chrome.storage.local.set({
+    magic: false
+  });
+  changeTrue();
+  MagicOption.style.display = 'none';
 });
 
 //zg First On/Off
-zgFirstElement.addEventListener('click',(e)=>{
-	var ison = e.target.getAttribute('checked');
-	if(ison){
-		chrome.storage.local.set({
-			zgFirst:false
-		});
-		changeTrue();
-		e.target.removeAttribute('checked');
-	} else {
-		chrome.storage.local.set({
-			zgFirst:true
-		});
-		changeTrue();
-		e.target.setAttribute('checked',true)
-	}
+zgFirstElement.addEventListener('click', e => {
+  var ison = e.target.getAttribute('checked');
+  if (ison) {
+    chrome.storage.local.set({
+      zgFirst: false
+    });
+    changeTrue();
+    e.target.removeAttribute('checked');
+  } else {
+    chrome.storage.local.set({
+      zgFirst: true
+    });
+    changeTrue();
+    e.target.setAttribute('checked', true);
+  }
 });
 
 //header on/off
-chrome.storage.local.get('disabledHeader',(result)=>{
-	disabledHeader = result.disabledHeader!==undefined ? result.disabledHeader : disabledHeader;
-	if(disabledHeader){
-		disabledHeaderElement.setAttribute('checked',true);
-		header.style.display = 'none';
-	} else {
-		header.style.display = 'block';
-	}
+chrome.storage.local.get('disabledHeader', result => {
+  disabledHeader =
+    result.disabledHeader !== undefined
+      ? result.disabledHeader
+      : disabledHeader;
+  if (disabledHeader) {
+    disabledHeaderElement.setAttribute('checked', true);
+    header.style.display = 'none';
+  } else {
+    header.style.display = 'block';
+  }
 });
 
 //set header
-MagicOption.addEventListener('submit',(e)=>{
-	uniHeader = uniHeaderElement.value ? uniHeaderElement.value : uniHeader;
-	zgHeader = zgHeaderElement.value ? zgHeaderElement.value : zgHeader;
-	chrome.storage.local.set({
-		uniHeader:uniHeader,
-		zgHeader:zgHeader
-	});
-	changeTrue();
+MagicOption.addEventListener('submit', e => {
+  uniHeader = uniHeaderElement.value ? uniHeaderElement.value : uniHeader;
+  zgHeader = zgHeaderElement.value ? zgHeaderElement.value : zgHeader;
+  chrome.storage.local.set({
+    uniHeader: uniHeader,
+    zgHeader: zgHeader
+  });
+  changeTrue();
 });
 
 //set disable header
-disabledHeaderElement.addEventListener('click',(e)=>{
-	var isdisable = e.target.getAttribute('checked');
-	if(isdisable){
-		chrome.storage.local.set({
-			disabledHeader:false
-		});
-		changeTrue();
-		header.style.display = 'block';
-		e.target.removeAttribute('checked');
-	} else {
-		chrome.storage.local.set({
-			disabledHeader:true
-		});
-		changeTrue();
-		header.style.display = 'none';
-		e.target.setAttribute('checked',true);
-	}
-})
+disabledHeaderElement.addEventListener('click', e => {
+  var isdisable = e.target.getAttribute('checked');
+  if (isdisable) {
+    chrome.storage.local.set({
+      disabledHeader: false
+    });
+    changeTrue();
+    header.style.display = 'block';
+    e.target.removeAttribute('checked');
+  } else {
+    chrome.storage.local.set({
+      disabledHeader: true
+    });
+    changeTrue();
+    header.style.display = 'none';
+    e.target.setAttribute('checked', true);
+  }
+});
